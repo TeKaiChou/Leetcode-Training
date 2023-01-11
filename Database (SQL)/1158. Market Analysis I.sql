cec80437388a38b928ef398d04011bb9a6fbc0f7
Kelvin Chou <61730268+TeKaiChou@users.noreply.github.com>
2023-01-10 19:26:59 -0800

    01/10/2023
    medium

---
+++ Database (SQL)/1158. Market Analysis I.sql
@@ -0,0 +1,4 @@
+select user_id as 'buyer_id', join_date, count(order_date) as 'orders_in_2019'
+from Users u
+left join Orders o on u.user_id = o.buyer_id and year(order_date) = 2019
+group by u.user_id
